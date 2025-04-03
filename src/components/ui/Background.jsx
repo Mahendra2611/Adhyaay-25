d93653e66943a02c3fb234a4d841428379b4578b
Kush Choudhary <kushchoudhary@Kushs-MacBook-Pro.local>
2025-04-03 19:53:17 +0530

    added events page [structure]

---
+++ src/components/ui/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ShootingStars } from './shooting-stars'
+import { StarsBackground } from './stars-background'
+
+export default function Background() {
+  return (
+    <div className="fixed -z-10 inset-0 bg-black">
+        <ShootingStars />
+        <StarsBackground />
+    </div>
+  )
+}
